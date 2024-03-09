779a04cbfefdbcf22a67ab6bde22e083c04835df
yosephbernandus <yosephbernandus@gmail.com>
2024-03-09 11:11:03 +0700

    add static root

--- src/billmate/settings.py
+++ src/billmate/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
