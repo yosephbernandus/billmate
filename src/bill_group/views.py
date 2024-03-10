320282c3019ced3b3a614fe3f51af3be490e62ea
yosephbernandus <yosephbernandus@gmail.com>
2024-03-10 10:19:28 +0700

    Remove fetch one

--- src/bill_group/views.py
+++ src/bill_group/views.py
@@ -15,15 +15,6 @@ def dictfetchall(cursor):
     return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 
-def dictfetchone(cursor):
-    """
-    Return all rows from a cursor as a dict.
-    Assume the column names are unique.
-    """
-    columns = [col[0] for col in cursor.description]
-    return [dict(zip(columns, row)) for row in cursor.fetchone()]
-
-
 @login_required
 def index(request):
     rows = []
