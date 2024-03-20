7037514c03e25ee0c97770ba6eee860be952ea44
yosephbernandus <yosephbernandus@gmail.com>
2024-03-20 07:22:40 +0700

    Feat: adding bill

--- src/bill_group/views.py
+++ src/bill_group/views.py
@@ -1,9 +1,11 @@
 from django.db import connection
 from django.shortcuts import render
 from django.utils import timezone
+from django.urls import reverse
 from django.shortcuts import render, redirect
 from bill_group.utils import create_public_group_link
 from base.utils import dictfetchall
+from django.contrib import messages
 
 from django.contrib.auth.decorators import login_required
 
@@ -124,11 +126,53 @@ def group_index(request, hash_ids: str):
 
 @login_required(login_url="")
 def add_bill(request, group_id):
+    user_id = request.user.id
+
+    if request.POST:
+        # TODO: Add condition to check group_id is related to user_id or not
+
+        name = request.POST.get('name')
+        subtotal = request.POST.get('subtotal')
+        total = request.POST.get('total')
+        tax = request.POST.get('tax')
+        fee = request.POST.get('fee')
+        discount = request.POST.get('discount')
+        description = request.POST.get('description')
+        paid_by = request.POST.get('paid_by')
+        participant_ids = request.POST.getlist('participants')
+
+        utc_now = timezone.now()
+
+        bill_id = None
+        query = """
+        INSERT INTO bill (name, sub_total_bill, total_bill, tax_rate, fee_rate, discount_rate, description, group_id, paid_by_participant_id, created_at, updated_at)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        RETURNING bill_id
+        """
+        with connection.cursor() as cursor:
+            cursor.execute(query, [
+                name, subtotal, total, tax, fee, discount, description, group_id, paid_by, utc_now, utc_now
+            ])
+            bill_id = cursor.fetchone()
+            bill_id = bill_id[0]
+
+            delete_query = "DELETE from bill_participant_owes where bill_id = %s"
+            cursor.execute(delete_query, [bill_id])
+
+            insert_data = [(bill_id, participant_id) for participant_id in participant_ids]
+            insert_query = """
+            INSERT INTO bill_participant_owes (bill_id, participant_id)
+            VALUES (%s, %s)
+            """
+            cursor.executemany(insert_query, insert_data)
+
+        return redirect(reverse('bill_group:group_bill_index', kwargs={'group_id': group_id}))
 
     participants = []
     with connection.cursor() as cursor:
         cursor.execute(
-            "select participant_id, name, group_id from participant where group_id = %s", [group_id]
+            "select p.participant_id, p.name, p.group_id from participant p join bill_group bg on bg.group_id = p.group_id where p.group_id = %s and bg.auth_user_id = %s",
+            [group_id, user_id]
         )
         participants = dictfetchall(cursor)
 
