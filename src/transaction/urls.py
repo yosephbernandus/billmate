21166c18c259c771ebf7f53a7118b1833b72c727
yosephbernandus <yosephbernandus@gmail.com>
2024-03-23 08:45:42 +0700

    Feat: Mark as paid transaction

--- src/transaction/urls.py
+++ src/transaction/urls.py
@@ -5,4 +5,5 @@ from transaction import views
 urlpatterns = [
     path("<int:bill_id>/add-transaction", views.add_transaction, name="add_transaction"),
     path("<int:transaction_id>/delete-transaction", views.delete_transaction, name="delete_transaction"),
+    path("<int:transaction_id>/mark-as-paid", views.mark_as_paid, name="mark_as_paid_transaction"),
 ]
